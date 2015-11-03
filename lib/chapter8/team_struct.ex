8b1cac7a90ee181706feea56e03244742b91ee02
David Schmitz <david.schmitz.privat@gmail.com>
2015-11-03 06:51:05 +0100

    Working on chapter 8

--- lib/chapter8/team_struct.ex
+++ lib/chapter8/team_struct.ex
@@ -1,5 +1,14 @@
-defmodule Chapter8 do
-  defmodule TeamStructure do
-    defstruct name: "", mascot: "", est: 0, division: "Regionalliga"
+
+defmodule TeamStructure do
+  defstruct name: "", mascot: "", est: 0, division: "Regionalliga"
+
+
+  def aufstieg(team = %TeamStructure{division: "1. Bundesliga"}) do
+    IO.puts "Deutscher Meister!!!"
+    team
+  end
+
+  def aufstieg(team = %TeamStructure{division: "2. Bundesliga"}) do
+    %{team | division: "1. Bundesliga"}
   end
 end
--- test/chapter8/team_struct_test.exs
+++ test/chapter8/team_struct_test.exs
@@ -2,14 +2,40 @@ defmodule TeamStructTest do
   use ExUnit.Case
 
 
-  alias Chapter8.TeamStructure
-
   test "creating a team" do
     teamA = %TeamStructure{}
     assert teamA == %TeamStructure{name: "", mascot: "", est: 0, division: "Regionalliga"}
 
     teamB = %TeamStructure{name: "Fortuna 1895", est: 1895, division: "2. Bundesliga"}
     assert teamB == %TeamStructure{name: "Fortuna 1895", mascot: "", est: 1895, division: "2. Bundesliga"}
+  end
+
+  test "field access" do
+    teamB = %TeamStructure{name: "Fortuna 1895", est: 1895, division: "2. Bundesliga"}
+
+    assert "Fortuna 1895" == teamB.name
+
+    %{est: est} =  teamB
+
+    assert 1895 == est
+  end
+
+  test "updates" do
+    teamB = %TeamStructure{name: "Fortuna 1895", est: 1895, division: "2. Bundesliga"}
+
+    teamB = %TeamStructure{teamB | division: "1. Bundesliga"}
+
+    assert teamB.division == "1. Bundesliga"
+  end
+
+
+  test "methods" do
+    teamB = %TeamStructure{name: "Fortuna 1895", est: 1895, division: "2. Bundesliga"}
+
+    res = teamB
+          |> TeamStructure.aufstieg
+
+    assert res.name == teamB.name and res.division == "1. Bundesliga"
   end
 
 end
