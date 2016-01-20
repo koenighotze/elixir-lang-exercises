f2c5120d7a2e05bcbfbc98817608513f6e03ea15
David Schmitz <david.schmitz.privat@gmail.com>
2016-01-20 10:19:41 +0100

    Finished String and binaries 5

---
+++ lib/chapter11/strings_and_binaries5.ex
@@ -0,0 +1,24 @@
+defmodule Chapter11.StringsAndBinaries5 do
+  def center([]), do: ""
+
+  def center(list), do: _center(list, max_length(list), "")
+  def _center([], width, aggregator), do: String.rstrip(aggregator, ?\n)
+  def _center([h | t], width, aggregator) do
+    pad = String.duplicate(" ", (width - String.length(h))  / 2 |> Float.floor |> round)
+
+    # todo use ljust(subject, len, padding)
+    padded = if String.length(h) == width do
+      h
+    else
+      "#{pad}#{h}"
+    end
+
+    _center(t, width, "#{aggregator}#{padded}\n")
+  end
+
+  def max_length([]), do: 0
+  def max_length([h | t]), do: _max_length(t, String.length(h))
+
+  defp _max_length([], current_max), do: current_max
+  defp _max_length([h | t], current_max), do: _max_length(t, max(current_max, String.length(h)))
+end
---
+++ test/chapter11/strings_and_binaries5_test.exs
@@ -0,0 +1,26 @@
+defmodule Chapter11.StringsAndBinaries5Test do
+  use ExUnit.Case
+
+  import Chapter11.StringsAndBinaries5, [:functions]
+
+  test "simple center" do
+    assert center(["foo"]) == "foo"
+  end
+
+  test "center" do
+    assert center(["cat", "zebra", "elephant"]) == "  cat\n zebra\nelephant"
+  end
+
+  test "center - with unicode" do                                                            
+    assert center(["∂⊃⊋∨≩≲≻", "⊋∨≩≲≻∂⊃⊋∨≩", "∂⊃⊋∨≩≲≻∂⊃⊋∨≩≲≻"]) == "   ∂⊃⊋∨≩≲≻\n  ⊋∨≩≲≻∂⊃⊋∨≩\n∂⊃⊋∨≩≲≻∂⊃⊋∨≩≲≻"
+  end
+
+  test "find max length - empty" do
+    assert max_length([]) == 0
+  end
+
+  test "find max length - single" do
+    assert max_length(["foo"]) == 3
+  end
+
+end
