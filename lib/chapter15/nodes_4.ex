de29a8430285e6bf81a69b58d185702ea08aec0d
David Schmitz <david.schmitz.privat@gmail.com>
2016-04-05 20:41:46 +0200

    Finished Nodes 4

--- lib/chapter15/nodes_4.ex
+++ lib/chapter15/nodes_4.ex
@@ -1,21 +1,71 @@
 defmodule Chapter15.Nodes4 do
-  import Chapter15.CircularList
+  defmodule Registry do
+    @registry :registry
 
-  def start do
-    pid = spawn(__MODULE__, :client_tick, [[]])
-  end
-
-  def register_client(pid) do
-
-  end
-
-  def client_tick(clients) do
-    receive do
-      {:register, pid} ->
-        client_tick([pid | clients])
-      {:status, self} ->
-        send self, %{clients: clients}
-        client_tick(clients)
+    def start do
+      pid = spawn_link(__MODULE__, :registry_master, [ nil, nil, [] ])
+      :global.register_name(@registry, pid)
     end
+
+    def inform_clients(new_client_pid, next_pid, first_pid) do
+      send new_client_pid, {:next, next_pid}
+      send first_pid, {:next, new_client_pid}
+    end
+
+    def registry_master(head, first, clients) do
+      receive do
+        {:register, client_pid} ->
+          new_first = if is_nil(first) do client_pid else first end
+          inform_clients(client_pid, head, new_first)
+
+          registry_master(client_pid, new_first, [ client_pid | clients ])
+        {:start_ticking} when [] != clients ->
+          send head, { :tick, self }
+          registry_master(head, first, clients)
+        {:status, sender} ->
+          status = %{clients: clients, head: head, first: first}
+          IO.puts "Sending status #{inspect status}"
+          send sender, status
+          registry_master(head, first, clients)
+      end
+    end
+
+    def status do
+      send :global.whereis_name(@registry), {:status, self}
+    end
+
+    def start_ticking do
+      send :global.whereis_name(@registry), {:start_ticking}
+    end
+  end
+
+  defmodule Client do
+    def ticker(next) do
+      receive do
+        {:next, pid} ->
+          IO.puts "Next for #{inspect self} is #{inspect pid}"
+          ticker(pid)
+        {:tick, _} when next != nil ->
+          :timer.sleep 5000
+          IO.puts "Tick #{inspect self} -> #{inspect next}"
+          send next, {:tick, self}
+          ticker(next)
+      end
+    end
+
+    def spawn_client do
+      client_pid = spawn_link(__MODULE__, :ticker, [ nil ])
+      send :global.whereis_name(:registry), {:register, client_pid}
+    end
+  end
+
+  def init do
+    Chapter15.Nodes4.Registry.start
+
+    1..5
+    |> Enum.to_list
+    |> Enum.each( fn _ -> Chapter15.Nodes4.Client.spawn_client end)
+
+    Chapter15.Nodes4.Registry.start_ticking
   end
 end
