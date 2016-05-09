1ddc9c8fd2c0ec963f6279d68cf5ce9f3c05c15b
David Schmitz <david.schmitz.privat@gmail.com>
2016-05-09 08:13:40 +0200

    Fixed test; disabled guard on def for tracer

--- lib/chapter21/linking_modules_behaviours_and_use_3.ex
+++ lib/chapter21/linking_modules_behaviours_and_use_3.ex
@@ -1,6 +1,6 @@
 defmodule Chapter21.LinkingModulesBehavioursAndUse3 do
   use Chapter21.Tracer
 
-  def add_three(x) when x < 5, do: x + 3
-  # def add_three(x), do: x + 3
+  # def add_three(x) when x < 5, do: x + 3
+  def add_three(x), do: x + 3
 end
