c34658a384e79b502f1a6015a53aaa69d957cacf
David Schmitz <david.schmitz.privat@gmail.com>
2016-08-09 08:27:45 +0200

    destructing the context arguments from setup

--- apps/exercises/test/chapter16/otp_servers_1_stack_server_test.exs
+++ apps/exercises/test/chapter16/otp_servers_1_stack_server_test.exs
@@ -6,9 +6,7 @@ defmodule Chapter16.OptServers1Test do
     {:ok, pid: pid}
   end
 
-  test "Poping returns the number", context do
-    pid = context[:pid]
-
+  test "Poping returns the number", %{pid: pid} do
     assert [4, 5, 6] == GenServer.call(pid, {:init, [4, 5, 6] })
 
     assert 4 == GenServer.call(pid, :pop)
@@ -16,14 +14,11 @@ defmodule Chapter16.OptServers1Test do
     assert 5 == GenServer.call(pid, :pop)
   end
 
-  test "popping an empty stack return nil", context do
-    pid = context[:pid]
-
+  test "popping an empty stack return nil", %{pid: pid} do
     assert nil == GenServer.call(pid, :pop)
   end
 
-  test "fetching the stats", context do
-    pid = context[:pid]
+  test "fetching the stats", %{pid: pid} do
     {:ok, data} = :sys.statistics pid, :get
 
     assert data[:messages_in] == 0
@@ -35,9 +30,7 @@ defmodule Chapter16.OptServers1Test do
     assert data[:messages_in] == 1
   end
 
-  test "get status", context do
-    pid = context[:pid]
-
+  test "get status", %{pid: pid} do
     {:status, ^pid, {:module, :gen_server}, _} = :sys.get_status pid
   end
 
--- apps/exercises/test/chapter16/otp_servers_2_stack_server_test.exs
+++ apps/exercises/test/chapter16/otp_servers_2_stack_server_test.exs
@@ -6,17 +6,13 @@ defmodule Chapter16.OptServers2Test do
     {:ok, pid: pid}
   end
 
-  test "numbers can be pushed to an empty stack", context do
-    pid = context[:pid]
-
+  test "numbers can be pushed to an empty stack", %{pid: pid} do
     GenServer.cast(pid, {:push, 7})
 
     assert 7 == GenServer.call(pid, :pop)
   end
 
-  test "numbers can be pushed to an nonempty empty stack", context do
-    pid = context[:pid]
-
+  test "numbers can be pushed to an nonempty empty stack", %{pid: pid} do
     assert [4, 5, 6] == GenServer.call(pid, {:init, [4, 5, 6] })
 
     GenServer.cast(pid, {:push, 7})
